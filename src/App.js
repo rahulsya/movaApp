32f59ba8e5bf1f1478af3c7d69d99e768bf645eb
rahulsya <rahulsyaban@gmail.com>
2022-11-17 23:36:35 +0700

    add config build release (code push)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+
 import {AuthStack, HomeStack} from './routes';
 import './i18n.config';
+
+import codePush from 'react-native-code-push';
+let codePushOptions = {checkFrequency: codePush.CheckFrequency.ON_APP_RESUME};
+
 const App = () => {
   return <HomeStack />;
 };
 
-export default App;
+export default codePush(codePushOptions)(App);
